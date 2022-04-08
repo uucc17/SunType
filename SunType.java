ccb40bc52e26bae934e36abc4ca29ced4f3015be
uucc17 <103113651+uucc17@users.noreply.github.com>
2022-04-08 22:55:55 +0900

    Add files via upload

---
+++ SunType.java
@@ -0,0 +1,9 @@
+public class SunType{
+  public static void main(String args[]){
+           int a = 10;
+           int b = 48;
+           double c = 7823.6;
+           double d = a+b+c;
+           System.out.println(d);
+ }
+}
